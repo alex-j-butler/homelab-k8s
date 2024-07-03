1cdbb5638db17427fc10f5a996cb452606865961
Alex Butler <alex@alex-j-butler.com>
2024-07-03 22:46:36 +1000

    Add Up-Actual-Webhook application.

---
+++ argocd/up-actual-webhook/helpers-config.js
@@ -0,0 +1,7 @@
+let config = {
+    "8ea921f6-6fe3-454b-89fb-809c22da6f7e": "a8233cd2-ff2a-4732-ad4b-71e13e54fc90", // Up Transactional
+    "dd597898-88de-4b2f-b55e-397857bbc35b": "5327eade-ac7b-4826-a7e1-6553eaa6d91f", // Up Income
+    "afbf32aa-ba4c-481f-b65a-85732f2ef16c": "d621b841-c404-4429-ac06-41b11bd77ee3", // Up Rainy Day
+}
+
+export default config;
